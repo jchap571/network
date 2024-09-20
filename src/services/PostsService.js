d150624c7ecfb8b23c556bee8665efeeb9883382
James Chapman <jchapman605@gmail.com>
2024-09-20 14:14:10 -0600

    pagination seems to be working, need to disable buttons on certain conditions

--- src/services/PostsService.js
+++ src/services/PostsService.js
@@ -25,6 +25,8 @@ class PostsService {
     logger.log('CHANGED POSTS PAGE', response.data)
 
     AppState.posts = newPosts
+    const newPostsPage = response.data.posts.map(postPOJO => new Post(postPOJO))
+    AppState.posts = newPostsPage
     AppState.page = response.data.page
     AppState.totalPages = response.data.totalPages
 
